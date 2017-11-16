e905dc5821d6bd86ddd5809b74ae61e722325bb1
Petulant Slacker <l.goohsen@gmail.com>
2017-11-16 17:22:31 -0500

    added rango to elementos

--- Rubricas_PCL/Rubrica/Categoria/Elemento/Elemento.cs
+++ Rubricas_PCL/Rubrica/Categoria/Elemento/Elemento.cs
@@ -13,6 +13,19 @@ namespace Rubricas_PCL
 		private string nivel3;
 		private string nivel4;
 
+        private float deNivel1;
+        private float hastaNivel1;
+
+		private float deNivel2;
+		private float hastaNivel2;
+
+		private float deNivel3;
+		private float hastaNivel3;
+
+		private float deNivel4;
+		private float hastaNivel4;
+
+
 		public string Uid
 		{
 			set
@@ -127,6 +140,134 @@ namespace Rubricas_PCL
 				}
 			}
 			get => nivel4;
+		}
+
+		public float DeNivel1
+		{
+			set
+			{
+				if (deNivel1 != value)
+				{
+					deNivel1 = value;
+					if (PropertyChanged != null)
+					{
+						PropertyChanged(this, new PropertyChangedEventArgs("DeNivel1"));
+					}
+				}
+			}
+			get => deNivel1;
+		}
+
+		public float HastaNivel1
+		{
+			set
+			{
+				if (hastaNivel1 != value)
+				{
+					hastaNivel1 = value;
+					if (PropertyChanged != null)
+					{
+						PropertyChanged(this, new PropertyChangedEventArgs("HastaNivel1"));
+					}
+				}
+			}
+			get => hastaNivel1;
+		}
+
+		public float DeNivel2
+		{
+			set
+			{
+				if (deNivel2 != value)
+				{
+					deNivel2 = value;
+					if (PropertyChanged != null)
+					{
+						PropertyChanged(this, new PropertyChangedEventArgs("DeNivel2"));
+					}
+				}
+			}
+			get => deNivel2;
+		}
+
+		public float HastaNivel2
+		{
+			set
+			{
+				if (hastaNivel2 != value)
+				{
+					hastaNivel2 = value;
+					if (PropertyChanged != null)
+					{
+						PropertyChanged(this, new PropertyChangedEventArgs("HastaNivel2"));
+					}
+				}
+			}
+			get => hastaNivel2;
+		}
+
+		public float DeNivel3
+		{
+			set
+			{
+				if (deNivel3 != value)
+				{
+					deNivel3 = value;
+					if (PropertyChanged != null)
+					{
+						PropertyChanged(this, new PropertyChangedEventArgs("DeNivel3"));
+					}
+				}
+			}
+			get => deNivel3;
+		}
+
+		public float HastaNivel3
+		{
+			set
+			{
+				if (hastaNivel3 != value)
+				{
+					hastaNivel3 = value;
+					if (PropertyChanged != null)
+					{
+						PropertyChanged(this, new PropertyChangedEventArgs("HastaNivel3"));
+					}
+				}
+			}
+			get => hastaNivel3;
+		}
+
+		public float DeNivel4
+		{
+			set
+			{
+				if (deNivel4 != value)
+				{
+					deNivel4 = value;
+					if (PropertyChanged != null)
+					{
+						PropertyChanged(this, new PropertyChangedEventArgs("DeNivel4"));
+					}
+				}
+			}
+			get => deNivel4;
+		}
+
+		public float HastaNivel4
+		{
+			set
+			{
+				if (hastaNivel4 != value)
+				{
+					hastaNivel4 = value;
+					if (PropertyChanged != null)
+					{
+						PropertyChanged(this, new PropertyChangedEventArgs("HastaNivel4"));
+					}
+				}
+			}
+			get => hastaNivel4;
 		}
 
 		public event PropertyChangedEventHandler PropertyChanged;
